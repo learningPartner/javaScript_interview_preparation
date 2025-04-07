5864a2980d6a7534d4289cdaa309172f18caa291
learningPartner <learningpartnerdigital@gmail.com>
2025-04-07 16:14:03 +0530

    sw

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ columns.forEach((col) => {
   const randomColor = colors[Math.floor(Math.random() * colors.length)]; // Pick a random color
   col.style.backgroundColor = randomColor;
 });
-const BASE_PATH = "javaScript_interview_preparation/";
+const BASE_PATH = "https://learningpartner.github.io/javaScript_interview_preparation/";
 const menu = [
   {
     name: "JavaScript",
